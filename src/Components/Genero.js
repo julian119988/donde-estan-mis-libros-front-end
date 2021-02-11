273a6e684e04c869fdb9ea7f383ccce69312548e
German <delucagerman@gmail.com>
2021-02-11 17:14:21 -0300

    se agrago reac-bootstrap y componentes de Persona, Libro, Genero

---
+++ src/Components/Genero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Genero = () => {
+    return (
+        <div>
+            <h2>Generos</h2>
+        </div>
+    )
+}
+
+export default Genero
---
+++ src/Components/Persona.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Persona = () => {
+    return (
+        <div>
+            <h2>Personas</h2>
+        </div>
+    )
+}
+
+export default Persona
