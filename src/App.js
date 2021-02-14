4684ddadbc437afdbb681dc9dde92b36ad82dd24
Julian Zambroni <julian.zambroni@gmail.com>
2021-02-14 20:18:25 -0300

    Agrege la peticion GET por axios

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { Image, Container } from "react-bootstrap";
+import Persona from "./Components/Persona";
+import Libro from "./Components/Libro";
+import Genero from "./Components/Genero";
+
+function App() {
+  return (
+    <div className="App">
+      <Container className="mt-5">
+        <h1>DONDE ESTAN MIS LIBROS</h1>
+        <Persona />
+        <Libro />
+        <Genero />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import ListadoPersona from "./Components/ListadoPersona/ListadoPersona";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ListadoPersona />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
