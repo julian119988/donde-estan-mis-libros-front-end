c9101361d1e5134ef949838b2d3441a39b6aff98
Julian Zambroni <julian.zambroni@gmail.com>
2021-02-17 17:27:29 -0300

    Un poco de bootstrap en creacion persona

--- src/Components/Persona/CreacionPersona/CreacionPersona.js
+++ src/Components/Persona/CreacionPersona/CreacionPersona.js
@@ -3,8 +3,6 @@ import React, { useRef, useState } from "react";
 import { useHistory } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
 
