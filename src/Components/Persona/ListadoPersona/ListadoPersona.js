a23b10a9756e40090afdc3eccd9a96e728e62992
Julian Zambroni <julian.zambroni@gmail.com>
2021-02-17 22:00:44 -0300

    BorrarItem ModificarPersona agregado, falta optimizar

--- src/Components/Persona/ListadoPersona/ListadoPersona.js
+++ src/Components/Persona/ListadoPersona/ListadoPersona.js
@@ -3,12 +3,14 @@ import ItemList from "../../ItemList/ItemList";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const ListadoPersona = () => {
+const ListadoPersona = (props) => {
   const [personas, setPersonas] = useState([""]);
+  const [actualizarLista, setActualizarLista] = useState(false);
 
   function fetchPersona() {
-    return axios.get("http://localhost:3001/persona").then((response) => {
+    return axios.get(props.url).then((response) => {
       setPersonas((personas) => {
+        console.log(response);
         return response.data;
       });
     });
@@ -22,24 +24,54 @@ const ListadoPersona = () => {
     //Esta funcion devuelve un id random al inicio ya que no puede definir undefined como key.
     return fila._id ? fila._id : Math.random(1000);
   }
+  function forzarEstado() {
+    fetchPersona();
+  }
+
+  function agregarBorrarModificar(key, index) {
+    const tamanioObjeto = Object.keys(personas[0]).length;
+    if (!(tamanioObjeto === index + 1)) {
+      return (
+        <th scope="col" key={Math.random(1000)}>
+          {key.charAt(0).toUpperCase() + key.slice(1)}
+        </th>
+      );
+    } else {
+      return (
+        <>
+          <th scope="col" key={Math.random(1000)}>
+            {key.charAt(0).toUpperCase() + key.slice(1)}
+          </th>
+          <th scope="col" key={Math.random(1000)}>
+            Borrar | Editar
+          </th>
+        </>
+      );
+    }
+  }
 
   return (
     <Table className="table-bordered table-hover">
       <thead className="thead-dark">
         <tr key="primerTr" className=".thead-dark">
           {Object.keys(personas[0]).map((key, index) => {
-            return (
-              // Se leen las keys de el primer item de la tabla, para armar la misma, luego se pone mayuscula la primer letra, (se tiene que hacer una peticion con axios)falta.
-              <th scope="col" key={key.toString() + index.toString()}>
-                {key.charAt(0).toUpperCase() + key.slice(1)}
-              </th>
-            );
+            return agregarBorrarModificar(key, index);
+
+            // Se leen las keys de el primer item de la tabla, para armar la misma, luego se pone mayuscula la primer letra, (se tiene que hacer una peticion con axios)falta.
           })}
         </tr>
       </thead>
       <tbody>
         {personas.map((fila) => {
-          return <ItemList fila={fila} key={returnId(fila)} />;
+          return (
+            <ItemList
+              fila={fila}
+              key={returnId(fila)}
+              url={props.url}
+              refresh={forzarEstado}
+              pagina="personas"
+            />
+          );
         })}
       </tbody>
     </Table>
