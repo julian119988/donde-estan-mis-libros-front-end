5d3fa29c4e18124642dc6392f757045d2047fc98
antomartini <anto-martini@hotmail.com>
2021-02-21 20:15:44 -0300

    Creacion de Genero/categoria

--- src/Components/Genero/Genero.js
+++ src/Components/Genero/Genero.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import CreacionGenero from './CreacionGenero/CreacionGenero'
+import Button from "react-bootstrap/Button";
+import { Link, Route, Switch } from "react-router-dom";
 
 const Genero = () => {
     return (
-        <div>
-            <h2>Generos</h2>
-        </div>
+        <>
+         <h1>Generos</h1>
+            <Link to="/generos/creacion">
+                <Button variant="danger">Crear Genero</Button>{" "}
+            </Link>
+            <Link to="/generos/listado">
+                <Button variant="danger">Listado de Generos</Button>{" "}
+            </Link>
+
+            <Switch>
+                <Route path="/generos/creacion">
+                    <CreacionGenero />
+                </Route>
+                <Route path="/generos/listado">
+                    
+                </Route>
+
+            </Switch>
+        </>
+
     )
 }
 
