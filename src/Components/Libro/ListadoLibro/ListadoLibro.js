41f723e4c9c3e7591673e583c7ae17e6d3928a4f
German <delucagerman@gmail.com>
2021-02-22 17:58:17 -0300

    listado de libros lista

--- src/Components/Libro/ListadoLibro/ListadoLibro.js
+++ src/Components/Libro/ListadoLibro/ListadoLibro.js
@@ -5,10 +5,33 @@ import { useEffect, useState } from "react";
 
 function ListadoLibro() {
 
+const [libros, setLibros] = useState([""]);
+
+const obtenerListadoDeLibros = async () => {
+    return axios.get("http://localhost:3001/libro").then((response) => {
+        setLibros(() => {
+            const libros = response.data;
+            console.log(libros);
+          return libros;
+          
+        });
+        //console.log(response.data);
+      });
+}
+
+useEffect(()=>{
+    obtenerListadoDeLibros();
+},[])
+
 
     
     return (
+        <>
         <h1>Listado de Libros</h1>
+        <ol>
+            {libros.map(l => <li key={l._id}>{l.nombre}</li>)}
+        </ol>
+        </>
     )
 }
 
