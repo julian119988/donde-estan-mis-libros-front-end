3cc524d40c067733dcf25a79e622e07f3f41b84c
Julian Zambroni <julian.zambroni@gmail.com>
2021-02-24 19:33:27 -0300

    Agregue navbar

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1>Hola esto es Home</h1>;
+}
--- src/Components/Persona/Persona.js
+++ src/Components/Persona/Persona.js
@@ -1,19 +1,28 @@
-import Button from "react-bootstrap/Button";
-import { Link, Route, Switch } from "react-router-dom";
+import { Link, Route, Switch, useHistory } from "react-router-dom";
 import ListadoPersona from "./ListadoPersona/ListadoPersona";
 import CreacionPersona from "./CreacionPersona/CreacionPersona";
 import ModificarPersona from "./ModificarPersona/ModificarPersona";
+
+import "./Persona.css";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+
+import Button from "react-bootstrap/Button";
 
 function Persona() {
   return (
     <>
       <h1>Personas</h1>
-      <Link to="/personas/creacion">
-        <Button variant="danger">Crear Persona</Button>{" "}
-      </Link>
-      <Link to="/personas/listado">
-        <Button variant="danger">Listado de Personas</Button>{" "}
-      </Link>
+
+      <ButtonGroup aria-label="Basic example">
+        <Link to="/personas/creacion">
+          <Button variant="danger">Crear Persona</Button>
+        </Link>
+        <Link to="/personas/listado">
+          <Button variant="danger" style={{ marginLeft: "-4px" }}>
+            Listado de personas
+          </Button>
+        </Link>
+      </ButtonGroup>
 
       <Switch>
         <Route path="/personas/creacion">
