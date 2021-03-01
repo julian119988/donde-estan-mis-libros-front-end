c43e388b85c2edba6902b8bbd30843147105e0ba
German <delucagerman@gmail.com>
2021-03-01 11:11:10 -0300

    Footer

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ import Card from "react-bootstrap/Card";
 import Navbar from "./Components/Navbar/Navbar";
 
 import Home from "./Components/Home/Home";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Container style={{ marginTop: "20px" }}>
+      <Container style={{ marginTop: "20px", height: "calc(100vh - 71px - 140px)" }}>
         <Card border="danger">
           <Card.Body>
             <Switch>
@@ -32,6 +33,7 @@ function App() {
           </Card.Body>
         </Card>
       </Container>
+        <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+
+const Footer = () => {
+    return (
+        <div className="bg-dark text-white text-center px-5 py-3 ">
+            <div className="row d-flex align-items-center">
+                <div className="col-md-4">
+                    <h5>Copyright © 2020 - { new Date().getFullYear() }</h5>
+                </div>
+                <div className="col-md-4">
+                    <h3>GRUPO 4:</h3>
+                </div>
+                <div className="col-md-4">
+                    <div className="row align-items-center">
+                       
+                        <div className="col justify-content-center text-left">
+                            <a href="https://github.com/julian119988" target="blank"><h6><img src="/github.png" alt="github" className="mr-3"/>Julian</h6></a>
+                            <a href="https://github.com/antomartini" target="blank"><h6><img src="/github.png" alt="github" className="mr-3"/>Antonella</h6></a>
+                            <a href="https://github.com/katerinesosaf" target="blank"><h6><img src="/github.png" alt="github" className="mr-3"/>Katerine</h6></a>
+                        </div>
+                        <div className="col justify-content-center text-left">
+                            <a href="https://github.com/delucagerman" target="blank"><h6><img src="/github.png" alt="github" className="mr-3"/>German</h6></a>
+                            <a href="https://github.com/delucagerman" target="blank"><h6><img src="/github.png" alt="github" className="mr-3"/>Gaby</h6></a>
+                        </div>
+                    </div>
+                
+                
+                    
+                   
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Footer
